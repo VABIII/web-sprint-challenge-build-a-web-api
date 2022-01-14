50183dc42d1cbce777b89f94a438ba2b9696c531
VABIII <tbengtson82@gmail.com>
2022-01-13 19:15:26 -0600

    project delete request working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,18 +2,60 @@ const express = require('express')
 
 const Projects = require('./projects-model')
 
-
-
 const router = express.Router()
 
+
+
 router.get('/', (req, res, next) =>{
-    Projects.get(req.params.id)
+    Projects.get()
         .then(projects => {
             res.json(projects)
         })
         .catch(next)
 
 })
+
+router.get('/:id', (req, res, next) => {
+
+    Projects.get(req.params.id)
+        .then(project => {
+            res.json(project)
+        })
+        .catch(next)
+
+})
+
+router.post('/', (req, res, next) => {
+    Projects.insert(req.body)
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(next)
+
+})
+
+router.put('/:id', (req, res, next) => {
+    const { id } = req.params
+    const changes = req.body
+    Projects.update(id, changes)
+        .then(projectUpdated => {
+            res.json(projectUpdated)
+        })
+        .catch(next)
+})
+
+router.delete('/:id', (req, res, next) =>{
+    const { id } = req.params
+
+    Projects.remove(id)
+        .then(deleted => {
+            res.json(deleted)
+        })
+        .catch(next)
+
+})
+
+
 
 router.get('/:id', (req, res, next) => {
     const {id} = req.params
