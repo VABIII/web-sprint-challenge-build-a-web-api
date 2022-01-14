02af7eb9207f96fd051c3793339f54ff47371e92
VABIII <tbengtson82@gmail.com>
2022-01-13 18:43:47 -0600

    Two project routes completed

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -6,9 +6,26 @@ const Projects = require('./projects-model')
 
 const router = express.Router()
 
+router.get('/', (req, res, next) =>{
+    Projects.get(req.params.id)
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+
+})
+
+router.get('/:id', (req, res, next) => {
+    const {id} = req.params
+
+    Projects.getProjectActions(id)
+        .then(actions => {
+            res.json(actions)
+        })
+        .catch(next)
 
 
-
+})
 
 
 
